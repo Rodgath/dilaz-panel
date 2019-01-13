d26ea6bd614a91f87e1784ee15822902fc845d43
Peter Rodgath <rodgath@gmail.com>
2019-01-13 10:25:15 +0300

    Update dilaz-panel.php

--- dilaz-panel.php
+++ dilaz-panel.php
@@ -4,7 +4,7 @@
  * Plugin URI:	http://webdilaz.com/plugins/dilaz-panel/
  * Description:	Simple options panel for WordPress themes and plugins.
  * Author:		WebDilaz Team
- * Version:		2.6.2
+ * Version:		2.6.1
  * Author URI:	http://webdilaz.com/
  * License:		GPL-2.0+
  * License URI:	http://www.gnu.org/licenses/gpl-2.0.txt
@@ -15,7 +15,7 @@
 ||
 || @package		Dilaz Panel
 || @subpackage	Panel
-|| @version		2.6.2
+|| @version		2.6.1
 || @since		Dilaz Panel 1.0
 || @author		WebDilaz Team, http://webdilaz.com
 || @copyright	Copyright (C) 2017, WebDilaz LTD
