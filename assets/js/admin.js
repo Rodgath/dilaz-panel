c54216ec53f9eadf3a90d72dd94402c3c7428045
Peter Rodgath <rodgath@gmail.com>
2019-06-08 07:01:05 +0300

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -434,11 +434,11 @@ var DilazPanelScript = new function() {
 						
 						$fileOutput += '</div>';
 						
-						if ($fileMultiple == true) {
+						// if ($fileMultiple == true) {
 							$fileWrapper.append($fileOutput);
-						} else {
-							$fileWrapper.html($fileOutput);
-						}
+						// } else {
+							// $fileWrapper.html($fileOutput);
+						// }
 						
 					});
 				});
