75f91a4b0127bb7ca4c500ff4a4842381f90c648
Peter Rodgath <rodgath@gmail.com>
2019-06-09 17:01:43 +0300

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1048,7 +1048,6 @@ var DilazPanelScript = new function() {
 					$headerLinkOutput += '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">';
 					$headerLinkOutput += '<link rel="preload" id="'+$linkId+'" href="'+fontURL+'" as="fetch" crossorigin="anonymous">';
 					$headerLinkOutput += '<script type="text/javascript"> !function(e,n,t){"use strict";var o="'+fontURL+'",r="__3perf_googleFonts_'+$linkId+'";function c(e){(n.head||n.body).appendChild(e)}function a(){var e=n.createElement("link");e.href=o,e.rel="stylesheet",c(e)}function f(e){if(!n.getElementById(r)){var t=n.createElement("style");t.id=r,c(t)}n.getElementById(r).innerHTML=e}e.FontFace&&e.FontFace.prototype.hasOwnProperty("display")?(t[r]&&f(t[r]),fetch(o).then(function(e){return e.text()}).then(function(e){return e.replace(/@font-face {/g,"@font-face{font-display:swap;")}).then(function(e){return t[r]=e}).then(f).catch(a)):a()}(window,document,localStorage); </script>';
-					$headerLinkOutput += '<!-- End of code snippet for Google Fonts -->';
 					
 					$('head>link:last').after($headerLinkOutput);
 					
