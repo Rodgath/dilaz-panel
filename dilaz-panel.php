c0f1d7301f63eb3645e4595df79728602dcbc7a6
Peter Rodgath <rodgath@gmail.com>
2019-06-12 21:06:16 +0300

    Update dilaz-panel.php

--- dilaz-panel.php
+++ dilaz-panel.php
@@ -151,7 +151,7 @@ if (!class_exists('DilazPanel')) {
 			$this->args        = is_array($option_args) ? $option_args : array();
 			$this->_params     = !empty($this->args) ? $this->sanitizeParams($this->args[0]) : array();
 			$this->_options    = !empty($this->args) ? $this->args[1] : array();
-			$this->_optionName = $this->_params['option_name'];
+			$this->_optionName = isset($this->_params['option_name']) ? $this->_params['option_name'] : '';
 			$this->_panelAtts  = $this->_options[0];
 			$saved_options     = $this->getOptions($this->_optionName);
 			$this->savedGFonts = isset($saved_options['saved_google_fonts']) ? $saved_options['saved_google_fonts'] : array();
