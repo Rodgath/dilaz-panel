a2deb37fcdb2327acb64a2de2c6db2a5af168543
Peter Rodgath <rodgath@gmail.com>
2019-06-13 04:20:28 +0300

    Update dilaz-panel.php

--- dilaz-panel.php
+++ dilaz-panel.php
@@ -1865,12 +1865,12 @@ if (!class_exists('DilazPanel')) {
 			
 				?>
 				
-				<!-- Code snippet to speed up Google Fonts rendering: googlefonts.3perf.com --> 
+				<!-- Code snippet to speed up Google Fonts rendering: googlefonts.3perf.com -->  
 				<link rel="dns-prefetch" href="https://fonts.gstatic.com"> 
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous"> 
 				<link rel="preload" href="<?php echo $font_url; ?>" as="fetch" crossorigin="anonymous">
-				<script type="text/javascript"> !function(e,n,t){"use strict";var o="<?php echo $font_url; ?>",r="__3perf_googleFonts_<?php echo 'dilaz'; ?>";function c(e){(n.head||n.body).appendChild(e)}function a(){var e=n.createElement("link");e.href=o,e.rel="stylesheet",c(e)}function f(e){if(!n.getElementById(r)){var t=n.createElement("style");t.id=r,c(t)}n.getElementById(r).innerHTML=e}e.FontFace&&e.FontFace.prototype.hasOwnProperty("display")?(t[r]&&f(t[r]),fetch(o).then(function(e){return e.text()}).then(function(e){return e.replace(/@font-face {/g,"@font-face{font-display:swap;")}).then(function(e){return t[r]=e}).then(f).catch(a)):a()}(window,document,localStorage); 
-				</script> 
+				<script type="text/javascript"> !function(e,n,t){"use strict";var o="<?php echo $font_url; ?>",r="__3perf_googleFonts_<?php echo (!empty($this->_optionName) ? $this->_optionName : 'dilaz'); ?>";function c(e){(n.head||n.body).appendChild(e)}function a(){var e=n.createElement("link");e.href=o,e.rel="stylesheet",c(e)}function f(e){if(!n.getElementById(r)){var t=n.createElement("style");t.id=r,c(t)}n.getElementById(r).innerHTML=e}e.FontFace&&e.FontFace.prototype.hasOwnProperty("display")?(t[r]&&f(t[r]),fetch(o).then(function(e){return e.text()}).then(function(e){return e.replace(/@font-face {/g,"@font-face{font-display:swap;")}).then(function(e){return t[r]=e}).then(f).catch(a)):a()}(window,document,localStorage); 
+				</script>
 				<!-- End of code snippet for Google Fonts -->
 				
 				<?php
