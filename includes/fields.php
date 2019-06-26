e83056b152566cc3319373af8b566ed5926f3dc5
Peter Rodgath <rodgath@gmail.com>
2019-06-26 17:19:58 +0300

    Fixed fieldRepeatable()

--- includes/fields.php
+++ includes/fields.php
@@ -263,14 +263,33 @@ if (!class_exists('DilazPanelFields')) {
 			
 			$output = '';
 			$output .= '<ul id="'. esc_attr($id) .'" class="dilaz-panel-repeatable '.$class.'" data-ns="'.$not_sortable.'" data-s="'.$sortable.'" data-nr="'.$not_removable.'" data-r="'.$removable.'">';
-				if (isset($options)) {
-					$i = 0;
+				$i = 0;	
+				if ($value != '') {
+					foreach($value as $key => $val) {
+						$output .= '<li class="dilaz-panel-repeatable-item">'.($not_sortable > $i ? '' : $sorter);
+							if (is_array($val)) {
+								foreach($val as $k => $v) {
+									$label = isset($options[0][$k]['label']) ? $options[0][$k]['label'] : '';
+									$field_size = isset($options[0][$k]['size']) ? intval($options[0][$k]['size']) : 30;
+									$output .= '<div class="dilaz-panel-repeatable-item-wrap inline">';
+									if ($label != '') {
+										$output .= '<label for="'.esc_attr($id).'"><strong>'.$label.'</strong></label>';
+									}
+									$output .= '<input type="text" class="'.$k.$i.'" name="'.esc_attr($id).'['.$i.'][]" value="'.$v.'" size="'.$field_size.'" />
+									</div>';
+								}
+							} else {
+								$output .= '<input type="text" name="'.esc_attr($id).'['.$i.']" value="'.$val.'" size="30" />';
+							}
+						$output .= ($not_removable > $i || $i < 1 ? '' : $remover).'</li>';
+						$i++;
+					}
+				} else {
 					foreach ((array)$options as $option_key => $option_value) {
 						$output .= '<li class="dilaz-panel-repeatable-item">'.($not_sortable > $i ? '' : $sorter);
 							if (is_array($option_value)) {
 								foreach($option_value as $k => $v) {
 									
-									$saved_value = isset($value[$option_key][$k]) ? $value[$option_key][$k] : $v['value'];
 									$label = isset($v['label']) ? $v['label'] : '';
 									$field_size = isset($options[0][$k]['size']) ? intval($options[0][$k]['size']) : 30;
 									
@@ -278,7 +297,7 @@ if (!class_exists('DilazPanelFields')) {
 									if ($label != '') {
 										$output .= '<label for="'.esc_attr($id).'"><strong>'.$v['label'].'</strong></label>';
 									}
-									$output .= '<input type="text" class="'.$k.$i.'" name="'.esc_attr($id).'['.$i.'][]" value="'.$saved_value.'" size="'.$field_size.'" />
+									$output .= '<input type="text" class="'.$k.$i.'" name="'.esc_attr($id).'['.$i.'][]" value="'.$v['value'].'" size="'.$field_size.'" />
 									</div>';
 								}
 							} else {
